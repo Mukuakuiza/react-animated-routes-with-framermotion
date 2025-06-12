d19c66703f3ed3fa0d5f2034947fc306479dd115
Mukuakuiza <hinomukuakuiza12@gmail.com>
2025-06-12 23:39:13 +0200

    created components and added style

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import './App.css'
+// import './App.css'
 import {BrowserRouter,NavLink,Routes,Route} from 'react-router-dom'
+import Header from './components/Header'
+import Home from './components/Home'
+import About from './components/About'
+import Services from './components/Services'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 function App() {
  
 
   return (
-    <>
-     <h1>Hello World</h1>
-    </>
+    <div className="App">
+      <Header />
+      <Home />
+      <About />
+      <Services />
+      <Contact />
+      <Footer />
+    </div>
   )
 }
 
---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+
+
+const About = () => {
+  return (
+     <div
+      className="about component"
+    >
+      <h1> About Component </h1>
+    </div>
+  )
+}
+
+export default About
